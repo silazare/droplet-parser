da7bd6f710163b9968cc3f5a46451ae80d4e9c28
Svyatoslav Lazarev <silazare.nsk@gmail.com>
2019-02-17 23:30:00 +0700

    Initial files addition

---
+++ README.md
@@ -0,0 +1,53 @@
+## Droplet parser
+
+![language](https://img.shields.io/badge/language-python-blue.svg)
+
+### Description:
+DigitalOcean Droplets info parsing tool:
+
+- Fetch ID for further DO API actions (reboot,power_cycle etc.)
+- Generate Private IP list in json dictionary according Droplet name
+- Generate DigitalOcean droplets inventory in dictionary
+- Handle as many DigitalOcean pages as needed, for large projects
+
+### How to use:
+
+- Clone this repository to your folder:
+```sh
+$ git clone https://github.com/silazare/droplet-parser.git
+$ cd droplet_parser
+```
+
+- Add your secret DO API key in env before use:
+```sh
+export DO_API_TOKEN=<secret_key>
+```
+
+- Execute to find ID:
+```sh
+$ python droplet_parser.py -ip <IPv4>
+Where: 
+      <IPv4> - IPv4 address in general octet format
+```
+
+- Execute to generate Private IP list:
+```sh
+$ python droplet_parser.py -f <NAME MASK>
+Where:
+      <NAME MASK> - Droplet name in general string format (ex. webserver-)
+```
+
+- Execute to generate droplets inventory list for your account:
+```sh
+$ python droplet_parser.py -list
+Output format:
+      { <droplet_name>: [<size>, <public ip>, <droplet id> ] }
+```
+
+## Additional options
+
+For large infrastructures you may use *-pages* argument with any other arguments, it works like you turn the pages in yout Digital Ocean web console. By default there is 1 page with 999 items on it, as far as I know it is maximum default.
+Example of usage:
+```sh
+$ python droplet_parser.py -list -pages 4
+```
---
+++ droplet_parser.py
@@ -0,0 +1,159 @@
+#!/usr/bin/python
+# DigitalOcean droplet information parser:
+# - Fetch Droplet ID by known Public IPv4
+#   Pass IPv4 as input parameters
+#   Usage example: python droplet_parser.py -ip X.X.X.X
+# - Fetch Private IP list in json format by known Droplet name
+#   Pass Droplet name or mask as input parameters
+#   Usage example: python droplet_parser.py -f webserver-
+# - Fetch DigitalOcean inventory dictionary for account
+#   Usage example: python droplet_parser.py -list
+# - Handle as many DigitalOcean pages as needed, for large projects.
+#   Usage example: python droplet_parser.py -list -pages <number>
+#
+#   DO API key should be set in following format:
+#   export DO_API_TOKEN=<secret_key>
+#
+# Author: Svyatoslav Lazarev (silazare.nsk@gmail.com)
+	
+	
+import os
+import sys
+import re
+import json
+import requests
+import argparse
+from sys import argv
+from pprint import pprint
+
+# API key environment check
+try:
+    api_key = os.environ['DO_API_TOKEN']
+except KeyError:
+    print('Set DO_API_TOKEN before use:')
+    print('export DO_API_TOKEN=<token>')
+    sys.exit(1)
+
+# Functions	
+def get_nest_data(url,token):
+    '''
+    function for API call to DigitalOcean
+    return raw dictionary in json format
+    '''
+    auth_t = token.encode("ascii", "ignore")
+    headers = {
+        'authorization': "Bearer " + auth_t,
+        'content-type': "application/json",
+    }
+
+    try:
+        init_res = requests.get(url, headers=headers, allow_redirects=False)
+        if init_res.status_code == 307:
+            api_response = requests.get(init_res, headers=headers, allow_redirects=False)
+            if api_response.status_code == 200:
+                return api_response.json()
+        elif init_res.status_code == 200:
+            return init_res.json()
+    except Exception as ce:
+        print(ce)
+
+def get_inventory(templates):
+    '''
+    function to get all droplets inventory from DigitalOcean
+    '''
+    hostname = {}   
+    for i in templates:
+        # j is a json dict of each droplet data
+        for j in templates[i]:
+            try:
+                inventory = []
+                for k in j:
+                    if k in ('name'):
+                        key = j[k].encode('ascii')
+                    elif k in ('id', 'size_slug'):
+                        try:
+                            inventory.append(j[k].encode('ascii'))
+                        except AttributeError:
+                            inventory.append(j[k])                         
+                    elif k in 'networks':
+                        inventory.append(j[k]['v4'][0]['ip_address'].encode('ascii'))
+                if key not in hostname:
+                    hostname[key] = inventory
+                else:
+                    hostname[key].append(inventory)
+            except TypeError:
+                continue
+    return(hostname)
+
+def get_id(templates,ipaddr):
+    '''
+    function to get droplet id by known Public IP
+    '''
+    for i in templates:
+        for j in templates[i]:
+           for k in j:
+               if k == 'id':
+                   if j['networks']['v4'][0]['ip_address'] == ipaddr:
+                      return j[k]
+
+def get_private_ip(templates,criteria):
+    '''
+    function to get Private IP list for known droplet name regex
+    '''
+    dict = {}
+    regex = ('^' + criteria)
+    for i in templates:
+            for j in templates[i]:
+                for k in j:
+                    if k == 'name':
+                        if re.match(regex, j[k]):
+                            if re.match('^10.\d{1,3}.\d{1,3}.\d{1,3}$', j['networks']['v4'][1]['ip_address']):
+                                dict[j[k].encode('ascii')] = [j['networks']['v4'][1]['ip_address'].encode('ascii')]
+                            else:
+                                dict[j[k].encode('ascii')] = [j['networks']['v4'][0]['ip_address'].encode('ascii')]
+    return(dict)
+	
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Digital Ocean droplets parse script')
+    parser.add_argument('-ip', action="store", dest="ip")
+    parser.add_argument('-f', action="store", dest="filter")
+    parser.add_argument('-list', action='store_true')
+    parser.add_argument('-pages', action="store", dest="pages", type=int, nargs='?', const=1, default=1)
+    args = parser.parse_args()
+
+#    link = "https://api.digitalocean.com/v2/droplets?page=1&per_page=999"
+    link = []
+    for item in range(1,args.pages+1): 
+        link.append("https://api.digitalocean.com/v2/droplets?page=" + str(item) + "&per_page=999")
+
+    if args.ip is not None:
+        result = None
+        try:
+            list_id = []
+            for k in link:
+                list_id.append(get_id(get_nest_data(k,api_key),args.ip))
+            filtered_list_id = filter(None, list(set(list_id)))
+            result = filtered_list_id[0]
+        except IndexError:
+            pass
+        except TypeError:
+            pass
+        if result is None:
+            print("No such droplet found in your account.")
+        else:
+            print(result)
+    elif args.filter is not None:
+        droplets = {}
+        for k in link:
+            droplets.update(get_private_ip(get_nest_data(k,api_key),args.filter))
+        if droplets:
+            pprint(droplets)
+        else:
+            print("No such droplet name found in your account.")
+    elif args.list:
+        hosts = {}
+        for k in link:
+            hosts.update(get_inventory(get_nest_data(k,api_key)))
+        pprint(hosts)
+    else:
+        print('Please pass -ip or -f or -list as input parameter')
